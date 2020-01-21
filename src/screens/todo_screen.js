d23b05f1c0ef442aa8f89ce5e37c44d281238788
vinej <jyvinet@hotmail.ca>
2020-01-20 21:35:06 -0500

    todo

--- src/screens/todo_screen.js
+++ src/screens/todo_screen.js
@@ -25,7 +25,7 @@ const TodoScreen = (props) => {
   return (
       <View>
         <View style= { { flexDirection: 'row'}}>
-          <Text style={ styles.title} >Getion des todos</Text>
+          <Text style={ styles.title} >Gestion des todos</Text>
           <Text style={ styles.index}> ({ TodoStore.getCount() },</Text>
           <Text style={ styles.index}>{ TodoStore.start() + 1 },</Text>
           <Text style={ styles.index}>{ TodoStore.end() } )</Text>
